caae7e08962aa31388afe6e8ea7820b180c13e16
mttaggart <michael.t.taggart@gmail.com>
2018-03-18 07:46:45 -0700

    Encrypt user passwords

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 const User = (sequelize, DataTypes) => {
     return sequelize.define("User", {
         firstName: {
@@ -16,6 +18,26 @@ const User = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false
         }
+    });
+}
+
+// Hook to encrypt user pws
+User.beforeCreate((user, options) => {
+    return encryptPassword(user.password)
+    .then(success => {
+        user.password = success;
+    })
+    .catch( err => {
+        if (err) console.log(err);
+    })
+});
+
+function encryptPassword(password) {
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password, 10, (err, hash) => {
+            if (err) return reject(err);
+            return resolve(hash);
+        });
     });
 }
 
