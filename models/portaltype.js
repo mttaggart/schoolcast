ab149935f1d8250e7472710dd1ce87a1258ac355
mttaggart <michael.t.taggart@gmail.com>
2018-03-18 15:19:16 -0700

    Portal Type typo fix

--- models/portaltype.js
+++ models/portaltype.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   PortalType.associate = function(models) {
     // associations can be defined here
-    models.PortalType.hasMnay(models.Portal);
+    models.PortalType.hasMany(models.Portal);
   };
   return PortalType;
 };
