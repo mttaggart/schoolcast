f4e74e39460bf2aef088c4669148eae4a6f68853
mttaggart <michael.t.taggart@gmail.com>
2018-03-18 17:38:47 -0700

    Feed test

---
+++ test/models/feed.test.js
@@ -0,0 +1,54 @@
+const assert = require("chai").assert;
+process.env.NODE_ENV = "test";
+const models = require("../../models");
+
+describe("Feed", () => {
+    before(done => {
+        models.Feed.sync({force: true})
+        .then(() => {
+            done();
+        });
+    });
+    describe("Create new", () => {
+        it("should create a new Feed", done => {
+            models.Feed.create({name: "test"})
+            .then( f => {
+                assert.equal(f.get("name"), "test");
+                done();
+            })
+            .catch(err => {
+                done(err);
+            });
+        });
+        it("should prevent null names", done => {
+            models.Feed.create({name:null})
+            .then( f => {
+                done(f.get("name"));
+            })
+            .catch( err => {
+                assert.exists(err);
+                done();
+            });
+        });
+    });
+    describe("Find", () => {
+        const testName = "Test Feed"
+        before( done => {
+            models.Feed.create({name: testName})
+            .then( f => {
+                done();
+            });
+        });
+        it("should find the test Feed", done => {
+            models.Feed.findOne({where: {name: testName}})
+            .then( f => {
+                assert.equal(f.get("name"),testName);
+                done();
+            })
+            .catch( err => {
+                assert.exists(err);
+                done(err);
+            });
+        });
+    });
+});
