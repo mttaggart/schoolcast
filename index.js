3f40be4bd22e0a5c0a36dee6f5e156d256fa9454
mttaggart <michael.t.taggart@gmail.com>
2018-03-21 17:22:40 -0700

    Add user routes

--- index.js
+++ index.js
@@ -9,9 +9,40 @@ app.set("port", (process.env.PORT || 3002));
 //Set up static routes
 router.use(express.static(__dirname + "/public"));
 
-router.get("/", function(req, res){
+router.route("/")
+.get((req, res) => {
   res.sendFile(path.resolve(__dirname,"public","index.html"));
 });
+
+router.route("/users")
+.get((req, res) => {
+  models.User.findAll()
+  .then(users => {
+    users.forEach( user => {
+      console.log(user);
+    })
+  })
+  .catch( err => {
+    console.log(err);
+    res.status(500).send("No users available");
+  });
+});
+
+router.route("/users/:id")
+.get((req, res) => {
+  models.User.findAll()
+  .then(users => {
+    users.forEach( user => {
+      console.log(user);
+    })
+  })
+  .catch( err => {
+    console.log(err);
+    res.status(500).send("No users available");
+  });
+});
+
+app.use(router);
 
 app.listen(app.get("port"), function() {
   console.log("Broadcasting on " + app.get("port"));
