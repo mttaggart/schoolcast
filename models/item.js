b2fd051e6937ab453331eb6d2131d9f9119d9bcd
mttaggart <michael.t.taggart@gmail.com>
2018-03-28 10:53:16 -0700

    Item fix

--- models/item.js
+++ models/item.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   var Item = sequelize.define('Item', {
     name: DataTypes.STRING,
-    content: DataTypes.Text
+    content: DataTypes.TEXT
   }, {});
   Item.associate = function(models) {
     // associations can be defined here
