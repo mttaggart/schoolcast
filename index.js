1cc445c23971ce7c9745deba1e192a48f6069519
mttaggart <michael.t.taggart@gmail.com>
2018-03-28 23:05:20 -0700

    User routes complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const models = require("./models");
 const app = express();
 const router = express.Router();
@@ -9,10 +10,15 @@ app.set("port", (process.env.PORT || 3002));
 //Set up static routes
 router.use(express.static(__dirname + "/public"));
 
+// Set up JSON middleware
+router.use(bodyParser.json());
+
 router.route("/")
 .get((req, res) => {
   res.sendFile(path.resolve(__dirname,"public","index.html"));
 });
+
+// USERS
 
 router.route("/api/users")
 .get((req, res) => {
@@ -24,18 +30,50 @@ router.route("/api/users")
     console.log(err);
     res.status(500).send("No users available");
   });
+})
+.post((req, res) => {
+  const userData = req.body;
+  models.User.create(userData)
+  .then( user => {
+    res.status(201).send(`${user.lastName}, ${user.firstName} created`);
+  })
+  .catch ( err => {
+    res.status(500).send("Could not create user");
+  });
 });
 
 router.route("/api/users/:id")
 .get((req, res) => {
-  models.User.findOne({id:req.params.id})
+  models.User.findOne({
+    where:{id:req.params.id}
+  })
   .then( user => {
-    res.status(200).send(user);
+    res.status(201).send(user);
   })
   .catch( err => {
     console.log(err);
     res.status(500).send("No users available");
   });
+})
+.put((req, res) => {
+  const userData = req.body;
+  models.User.update(userData,{where: {id: req.params.id}})
+  .then( rows => {
+    res.status(201).send(rows);
+  })
+  .catch( err => {
+    res.status(500).send("Could not update");
+  });
+})
+.delete((req, res) => {
+  const userId = req.params.id;
+  models.User.destroy({where: {id: userId}})
+  .then ( rows => {
+    res.status(200).send("User deleted");
+  })
+  .catch( err => {
+    res.status(500).send("Could not delete user");
+  })
 });
 
 app.use(router);
