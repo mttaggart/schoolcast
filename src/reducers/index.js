6431c777683f472ef3017b54a42550e9a2b3659f
mttaggart <michael.t.taggart@gmail.com>
2018-04-04 22:16:20 -0700

    Add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
 import { combineReducers } from "redux";
-import  authReducer from "./authReducer";
+import authReducer from "./authReducer";
+import displaysReducer from "./displaysReducer";
+import feedsReducer from "./feedsReducer";
+import itemsReducer from "./itemsReducer";
+import portalsReducer from "./portalsReducer";
 import usersReducer from "./usersReducer";
 
 const reducer = combineReducers({
     auth: authReducer,
+    displays: displaysReducer,
+    feeds: feedsReducer,
+    items: itemsReducer,
+    portals: portalsReducer,
     users: usersReducer
 });
 
