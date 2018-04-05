045ad746666d38ce362e616929e87678b441b2d4
mttaggart <michael.t.taggart@gmail.com>
2018-04-04 21:51:34 -0700

    Add feed actions

---
+++ src/actions/feeds/deleteFeed.js
@@ -0,0 +1,42 @@
+import actionTypes from "./actionTypes";
+const apiEndpoint = "/api/feeds/";
+
+export default function deleteFeed(token, id) {
+    return (dispatch) => {
+        dispatch(deleteFeedAttempted());
+        const deleteRequest = new Request(
+            apiEndpoint + id,
+            {
+                method: "DELETE",
+                headers: {
+                    "x-access-token": token
+                }
+            }
+        );
+        fetch(deleteRequest)
+        .then( res => res.json())
+        .then( json => dispatch(deleteFeedSucceeded(json)))
+        .catch( err => dispatch(deleteFeedRejected(err)));
+    }
+}
+
+
+function deleteFeedAttempted() {
+    return {
+        type: actionTypes.DELETE_FEED_ATTEMPTED
+    }
+}
+
+function deleteFeedRejected(err) {
+    return {
+        type: actionTypes.DELETE_FEED_REJECTED,
+        data: err
+    }
+}
+
+function deleteFeedSucceeded(token, data) {
+    return {
+        type: actionTypes.DELETE_FEED_SUCCEEDED,
+        data
+    }
+}
---
+++ src/actions/feeds/index.js
@@ -0,0 +1,11 @@
+import getFeeds from "./getFeeds";
+import addFeed from "./addFeed";
+import deleteFeed from "./deleteFeed";
+import updateFeed from "./updateFeed";
+
+export default {
+    getFeeds,
+    addFeed,
+    deleteFeed,
+    updateFeed
+}
