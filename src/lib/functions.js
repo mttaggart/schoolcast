b027adcec35e3e92d4d3523e5109d8c5b1253dcc
mttaggart <michael.t.taggart@gmail.com>
2018-06-07 22:03:21 -0700

    Pass state for new items

--- src/lib/functions.js
+++ src/lib/functions.js
@@ -1,4 +1,9 @@
 export function deriveById(nextProps, prevState, defaultItem) {
+
+    // Let state pass through on new form
+    if(nextProps.match.path.endsWith("new")) return prevState;
+
+    // If editing an existing asset, check if it exists
     if (nextProps.match.params.id) {
         const  id = parseInt(nextProps.match.params.id,10);
         if(id === prevState.id) return prevState;
@@ -7,5 +12,6 @@ export function deriveById(nextProps, prevState, defaultItem) {
         });
         return item ? item : defaultItem;
     }
+
     return defaultItem;
 }
