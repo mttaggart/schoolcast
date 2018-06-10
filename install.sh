ac994a658ca8e22029f08de5dc673f30b77f30d6
mttaggart <michael.t.taggart@gmail.com>
2018-06-10 00:54:50 -0700

    Finish install script

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Use yarn if available
+if [ -e /usr/bin/yarn ]; then
+    echo "Installing dependencies with Yarn"
+    PM=yarn
+# Otherwise use NPM
+elif [ -e /usr/bin/npm ]; then
+    echo "Installing dependencies with NPM"
+    PM=npm
+# Install these things before running this script!
+else
+    echo "Cannot install dependencies."
+    echo "Please install yarn (https://yarnpkg.com) or node and use npm."
+    exit -1
+fi
+
+# Actually install deps
+$PM install 
+
+# Build the app
+$PM build
+
 if [ -e ./schoolcast.conf ]; then
     echo "Found config file; starting..."
     source ./schoolcast.conf
@@ -8,39 +29,16 @@ else
     exit -1
 fi
 
-
-# Use yarn if available
-if [ -e /usr/bin/yarn ]; then
-    echo "Installing dependencies with Yarn"
-    yarn install;
-# Otherwise use NPM
-elif [ -e /usr/bin/npm ]; then
-    echo "Installing dependencies with NPM"
-    npm install;
-# Install these things before running this script!
-else
-    echo "Cannot install dependencies."
-    echo "Please install yarn (https://yarnpkg.com) or node and use npm."
-    exit -1
-fi
-
 SQ_PATH=./node_modules/.bin/sequelize
-
-# Set Environment
-if [ "$NODE_ENV" == "production" ]; then
-    ENVIRONMENT="production";
-else
-    ENVIRONMENT="development"
-fi
 
 echo "Using $ENVIRONMENT environment for deployment."
 
 # Run Migrations
 echo "Running migrations"
-$SQ_PATH db:migrate --env=$ENVIRONMENT
+$SQ_PATH db:migrate --env=$NODE_ENV
 
 # Run Seeds
 echo "Seeding Database"
-$SQ_PATH db:seed:all --env=$ENVIRONMENT
+$SQ_PATH db:seed:all --env=$NODE_ENV
 
 exit 0
