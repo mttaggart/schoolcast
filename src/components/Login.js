3e35ca69e663d5dac655bd0820eaf533525efb13
mttaggart <michael.t.taggart@gmail.com>
2018-06-09 17:37:22 -0700

    Style login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
-import { Label, Button, Intent } from "@blueprintjs/core";
+import { ButtonGroup, Button, Intent } from "@blueprintjs/core";
+import { TextInput } from "./FormControls";
+
+const styles = {
+    loginForm: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center"
+    }
+}
 
 class Login extends React.Component {
     constructor(props) {
@@ -36,18 +46,31 @@ class Login extends React.Component {
         return (
             <div>
                 <h2>Login</h2>
-                <form onSubmit={this.onSubmit.bind(this)}>
-                    {/* {this.props.error ? <div>{this.props.error}</div> : null} */}
-                    <Label>email</Label>
-                    <input className="pt-input" onChange={this.emailHandler.bind(this)} id="email" type="email" />
-                    <Label>password</Label>
-                    <input className="pt-input" onChange={this.pwHandler.bind(this)} id="password" type="password" />
-                    <Button 
-                        type="submit"
-                        intent={Intent.PRIMARY}
-                        text="Log In"
-                        icon="login" 
+                <form 
+                    onSubmit={this.onSubmit.bind(this)}
+                    style={styles.loginForm}
+                >
+                    {this.props.error ? <div>{this.props.error}</div> : null}
+                    <TextInput 
+                        label="Email"
+                        type="email"
+                        id="email"
+                        changeHandler={this.emailHandler.bind(this)}
                     />
+                    <TextInput 
+                        label="Password"
+                        type="password"
+                        id="password"
+                        changeHandler={this.pwHandler.bind(this)}
+                    />
+                    <ButtonGroup large={true}>
+                        <Button 
+                            type="submit"
+                            intent={Intent.PRIMARY}
+                            text="Log In"
+                            icon="login" 
+                        />
+                    </ButtonGroup>
                 </form>
             </div>
         );
