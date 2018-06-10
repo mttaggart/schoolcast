2e600831d5f5dfd280c4fe02f0fcf8b9492be2d3
mttaggart <michael.t.taggart@gmail.com>
2018-06-10 10:54:02 -0700

    Add space

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <Callout title="Welcome to SchoolCast">
                 <p>
                     This system is designed for the easy creation of informational signage in educational settings. Use the 
-                    <b>Admin</b> section to configure Displays, Feeds, Portals, 
+                    <b> Admin</b> section to configure Displays, Feeds, Portals, 
                     and Items. Then, use the <b>Displays</b> section to access
                     your Displays!
                 </p>
