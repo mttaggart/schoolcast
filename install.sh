1b7fe4dec54b63973d52b0baf448dcb608133149
mttaggart <michael.t.taggart@gmail.com>
2018-06-10 00:14:10 -0700

    Check for config file

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ -e ./schoolcast.conf ]; then
+    echo "Found config file; starting..."
+    source ./schoolcast.conf
+else
+    echo "Config file not found. Exiting..."
+    exit -1
+fi
+
+
 # Use yarn if available
 if [ -e /usr/bin/yarn ]; then
     echo "Installing dependencies with Yarn"
