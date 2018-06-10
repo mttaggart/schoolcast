b33a6aaca69fd35a0431e40d85fdf7aca308462e
mttaggart <michael.t.taggart@gmail.com>
2018-06-10 13:39:56 -0700

    HTML-enabled text

--- src/components/DisplaysView/PortalTypes/TextPortal.js
+++ src/components/DisplaysView/PortalTypes/TextPortal.js
@@ -1,11 +1,13 @@
 import React from "react";
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import { assignPortalCss } from "../../../lib/functions";
+
 
 const TextPortal = ({portal, item}) => {
     const styles = assignPortalCss(portal);
     return (
         <div style={styles}>
-            <p>{item.content}</p>
+            {ReactHtmlParser(item.content)}
         </div>
     )
 }
